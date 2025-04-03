abc1aeec36ce1b95c3d1666cf4bda411abf22bdb
Sandeep Raina <127074583+Sandeepraina40@users.noreply.github.com>
2025-04-03 20:07:33 +0530

    Update PaginationButtons.jsx

--- src/components/PaginationButtons.jsx
+++ src/components/PaginationButtons.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 
-export default function https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout() {
+export default function PaginationButtons() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const searchTerm = searchParams.get('searchTerm');
