58bfadc9a5aab651742f757da8f7a1a258f21507
Sandeepraina40 <127074583+Sandeepraina40@users.noreply.github.com>
2025-04-03 14:43:13 +0530

    added the remaining part of the code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Footer from "@/components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import './globals.css';
+import Footer from '@/components/Footer';
 
 export const metadata = {
-  title: "Google Clone",
-  description: "An open source Google Clone Create with Nextjs and TailwindCSS",
+  title: 'Google Clone',
+  description:
+    'An open source Google clone built with Next.js and Tailwind CSS.',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang='en'>
+      <body className='relative min-h-screen'>
         {children}
         <Footer />
       </body>
---
+++ src/components/ImageSearchREsults.jsx
@@ -0,0 +1,5 @@
+export default function ImageSearchResults() {
+    return(
+        <div>ImageSerachResults</div>
+    )
+}
